d5718e9d925582be98c4f73f18ed6078d5a3054f
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-12 22:19:06 -0700

    Draw 5 cards

--- src/main/java/EuchreGame.java
+++ src/main/java/EuchreGame.java
@@ -9,6 +9,8 @@ public class EuchreGame extends JComponent {
     public static final int CARD_WIDTH = 70;
     public static final Color CARD_FILL = new Color(241, 239, 228);
     public static final Color CARD_BORDER = new Color(40, 38, 35);
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Euchre");
@@ -21,17 +23,23 @@ public class EuchreGame extends JComponent {
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(800, 600);
+        return new Dimension(GAME_WIDTH, GAME_HEIGHT);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         g.setColor(TABLE_COLOR);
-        g.fillRect(0, 0, 800, 600);
+        g.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
 
+        for (int i = 0; i < 5; i++) {
+            drawCard(g, 200 + (CARD_WIDTH + 10) * i, GAME_HEIGHT - CARD_HEIGHT - 30);
+        }
+    }
+
+    private void drawCard(Graphics g, int x, int y) {
         g.setColor(CARD_FILL);
-        g.fillRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, CARD_CORNER, CARD_CORNER);
+        g.fillRoundRect(x, y, CARD_WIDTH, CARD_HEIGHT, CARD_CORNER, CARD_CORNER);
         g.setColor(CARD_BORDER);
-        g.drawRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, CARD_CORNER, CARD_CORNER);
+        g.drawRoundRect(x, y, CARD_WIDTH, CARD_HEIGHT, CARD_CORNER, CARD_CORNER);
     }
 }
