81e98e1562aa0d8c5da3bdeac946dcdbf10bb01c
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-12 22:15:10 -0700

    Draw a card

--- src/main/java/EuchreGame.java
+++ src/main/java/EuchreGame.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class EuchreGame extends JComponent {
+
+    public static final Color TABLE_COLOR = new Color(204, 185, 159);
+    public static final int CARD_CORNER = 7;
+    public static final int CARD_HEIGHT = 100;
+    public static final int CARD_WIDTH = 70;
+    public static final Color CARD_FILL = new Color(241, 239, 228);
+    public static final Color CARD_BORDER = new Color(40, 38, 35);
+
     public static void main(String[] args) {
         JFrame window = new JFrame("Euchre");
         window.add(new EuchreGame());
@@ -18,7 +26,12 @@ public class EuchreGame extends JComponent {
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.setColor(Color.RED);
+        g.setColor(TABLE_COLOR);
         g.fillRect(0, 0, 800, 600);
+
+        g.setColor(CARD_FILL);
+        g.fillRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, CARD_CORNER, CARD_CORNER);
+        g.setColor(CARD_BORDER);
+        g.drawRoundRect(0, 0, CARD_WIDTH, CARD_HEIGHT, CARD_CORNER, CARD_CORNER);
     }
 }
